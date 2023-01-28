d8efce9c28ef849c623807542d1ef2ddc633f29f
Hemant Kumar Meena <meenahemantkr@gmail.com>
2023-01-28 12:35:52 +0530

    SSH Deploy changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,9 @@
                         </g>
                     </svg>
                 </div>
+                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                    <h5>Delpoyed via SSH</h5>
+                </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
