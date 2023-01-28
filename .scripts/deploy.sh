7a4687c12096da441f45da3771edf54fa435a056
Hemant Kumar Meena <meenahemantkr@gmail.com>
2023-01-28 12:27:28 +0530

    Script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,29 +3,27 @@ set -e
 
 echo "Deployment started ..."
 
-# Enter maintenance mode or return true
+# Turn ON Maintenance Mode or return true
 # if already is in maintenance mode
 (php artisan down) || true
 
 # Pull the latest version of the app
-git pull origin production
+git pull origin master
 
 # Install composer dependencies
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+composer install --optimize-autoloader --no-dev --no-interaction
 
-# Clear the old cache
-php artisan clear-compiled
+# Clearing Cache
+php artisan cache:clear
+php artisan config:clear
 
 # Recreate cache
 php artisan optimize
 
-# Compile npm assets
-#npm run prod
-
 # Run database migrations
 php artisan migrate --force
 
-# Exit maintenance mode
+# Turn OFF Maintenance mode
 php artisan up
 
 echo "Deployment finished!"
