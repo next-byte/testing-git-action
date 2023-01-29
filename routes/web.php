fe5beb66285751c00c6a967ab5bd5bb4761b2226
Hemant Kumar Meena <meenahemantkr@gmail.com>
2023-01-29 10:59:46 +0530

    wip 5

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Deployed via ssh & artifacts.\n Multi-server deployment testing.";
+//    return view('welcome');
 });
